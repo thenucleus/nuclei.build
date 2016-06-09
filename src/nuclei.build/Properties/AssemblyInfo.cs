cd3bbb89d2cf24f21ea5bd5f5e2b7af7d1df8e31
Petrik van der Velde <petrikvandervelde@gmail.com>
2016-06-09 18:35:26 +1200

    Updating the assemblyinfo file to have all the required attributes
    references #2

--- src/nuclei.build/Properties/AssemblyInfo.cs
+++ src/nuclei.build/Properties/AssemblyInfo.cs
@@ -6,6 +6,7 @@
 
 using System.Reflection;
 using System.Resources;
+using Nuclei.Build;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
@@ -13,7 +14,32 @@ using System.Resources;
 [assembly: AssemblyTitle("Nuclei.Build")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyProduct("Nuclei.Build")]
+
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyCopyright("Copyright ©  2013")]
+
 [assembly: AssemblyCulture("")]
 
 // Resources
 [assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.MainAssembly)]
+
+[assembly: AssemblyConfiguration("")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+// The time the assembly was build
+[assembly: AssemblyBuildTime(buildTime: "1900-01-01T01:01:01+00:00")]
+
+// The version from which the assembly was build
+[assembly: AssemblyBuildInformation(buildNumber: 0, versionControlInformation: "1234567890abcdef1234567890abcdef12345678")]

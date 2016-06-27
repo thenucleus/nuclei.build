be9c62b9c24420912b3a7283590753a28f5ae4ff
Petrik van der Velde <petrikvandervelde@gmail.com>
2016-06-27 22:31:23 +1200

    Fixing the build

--- src/nuclei.build/Properties/AssemblyInfo.cs
+++ src/nuclei.build/Properties/AssemblyInfo.cs
@@ -5,8 +5,11 @@
 // </copyright>
 //-----------------------------------------------------------------------
 
+using System;
+using System.Diagnostics.CodeAnalysis;
 using System.Reflection;
 using System.Resources;
+using System.Runtime.InteropServices;
 using Nuclei.Build;
 
 // General Information about an assembly is controlled through the following
@@ -24,7 +27,20 @@ using Nuclei.Build;
 // Resources
 [assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.MainAssembly)]
 
-[assembly: AssemblyConfiguration("")]
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// Indicate that the assembly is CLS compliant.
+[assembly: CLSCompliant(true)]
+
+[assembly: AssemblyConfiguration("Release")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -39,3 +55,13 @@ using Nuclei.Build;
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: AssemblyInformationalVersion("1.0.0.0")]
+
+// The time the assembly was build
+[assembly: AssemblyBuildTime(buildTime: "1900-01-01T00:00:00.0000000+00:00")]
+
+// The version from which the assembly was build
+[module: SuppressMessage(
+    "Microsoft.Usage",
+    "CA2243:AttributeStringLiteralsShouldParseCorrectly",
+    Justification = "It's a VCS revision, not a version")]
+[assembly: AssemblyBuildInformation(buildNumber: 0, versionControlInformation: "1234567890123456789012345678901234567890")]
